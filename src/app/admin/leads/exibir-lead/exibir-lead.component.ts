79331c358dcd8def12782ffd928eca02c0fbb071
Eduardo Rangell <rangellferreira@gmail.com>
2021-04-12 22:32:23 -0300

    Exibir, Criar e Editar Lead junto com a importação no módulo

---
+++ src/app/admin/leads/exibir-lead/exibir-lead.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exibir-lead',
+  templateUrl: './exibir-lead.component.html',
+  styleUrls: ['./exibir-lead.component.scss']
+})
+export class ExibirLeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
