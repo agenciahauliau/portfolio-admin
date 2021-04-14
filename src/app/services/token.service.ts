ca117c4340f99b985c121066ba5a19c598e0902d
Eduardo Rangell <rangellferreira@gmail.com>
2021-04-14 04:42:52 -0300

    Remoção de importações desnecessárias

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { BehaviorSubject } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { GQL_CHECK } from '../graphql/graphql';
 
 @Injectable({
   providedIn: 'root',
