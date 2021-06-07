baf0d348b079a7b8f8aa69fa833b2b545d602b41
Eduardo Rangell <rangellferreira@gmail.com>
2021-06-07 19:21:50 -0300

    Remoção do ngx-viacep compilado manualmente e ng update

--- ngx-viacep/lib/ngx-viacep.module.d.ts
+++
@@ -1,9 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from '@angular/common/http';
-export declare class NgxViacepModule {
-    static ɵfac: ɵngcc0.ɵɵFactoryDef<NgxViacepModule, never>;
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NgxViacepModule, never, [typeof ɵngcc1.HttpClientModule], never>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<NgxViacepModule>;
-}
-
-//# sourceMappingURL=ngx-viacep.module.d.ts.map
