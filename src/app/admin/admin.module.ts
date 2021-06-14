faf11d0e271fb5a3559d77280f6403309b11e1ee
Eduardo Rangell <rangellferreira@gmail.com>
2021-06-14 20:42:11 -0300

    Sort type e paginação

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { NgxViacepModule } from '@brunoc/ngx-viacep';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxCurrencyModule } from 'ngx-currency';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 
 import { AdminComponent } from './admin.component';
@@ -22,6 +24,13 @@ import { ListarLeadsComponent } from './leads/listar-leads/listar-leads.componen
 import { CriarLeadComponent } from './leads/criar-lead/criar-lead.component';
 import { ExibirLeadComponent } from './leads/exibir-lead/exibir-lead.component';
 import { EditarLeadComponent } from './leads/editar-lead/editar-lead.component';
+
+import { ExibirPostComponent } from './blog/exibir-post/exibir-post.component';
+import { EditarPostComponent } from './blog/editar-post/editar-post.component';
+import { CriarPostComponent } from './blog/criar-post/criar-post.component';
+import { ListarPostsComponent } from './blog/listar-posts/listar-posts.component';
+import { TruncatePipe } from '../helpers/truncate.pipe';
+import { SortDirective } from '../helpers/sort.directive';
 @NgModule({
   declarations: [
     AdminComponent,
@@ -35,6 +44,12 @@ import { EditarLeadComponent } from './leads/editar-lead/editar-lead.component';
     CriarLeadComponent,
     ExibirLeadComponent,
     EditarLeadComponent,
+    ExibirPostComponent,
+    EditarPostComponent,
+    CriarPostComponent,
+    ListarPostsComponent,
+    TruncatePipe,
+    SortDirective,
   ],
   imports: [
     CommonModule,
@@ -46,6 +61,8 @@ import { EditarLeadComponent } from './leads/editar-lead/editar-lead.component';
     NgxMaskModule.forRoot(),
     EditorModule,
     NgxViacepModule,
+    NgxPaginationModule,
+    NgxCurrencyModule,
   ],
   providers: [{ provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }],
 })
