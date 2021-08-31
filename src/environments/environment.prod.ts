45c0b7aa5c8310e0ae99cad97d1715d2091e6c53
Eduardo Rangell <rangellferreira@gmail.com>
2021-08-31 13:38:09 -0300

    Fix: mudando para o backend no Google App Engine até nova resolução.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  API: 'https://api.portfolio.imb.br/v1/',
+  //API: 'https://api.portfolio.imb.br/v1/',
+  API: 'https://back-portfolio-imb-br-dot-rangell-consultoria-ti.rj.r.appspot.com/v1/',
   TOKEN_KEY: 'auth-token',
   USER_KEY: 'auth-user',
 };
